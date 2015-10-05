f41c643cb29f4152b3ac17c82e5ed1a836918c95
Willian Justen <willianjustenqui@gmail.com>
2015-10-05 18:36:08 -0300

    Add gulp-cache to gulp-imagemin process

--- gulp/tasks/imagemin.js
+++ gulp/tasks/imagemin.js
@@ -3,13 +3,14 @@
 // Necessary Plugins
 var gulp      = require('gulp')
     ,plumber  = require('gulp-plumber')
-    ,paths = require('../paths')
-    ,imagemin = require('gulp-imagemin');
+    ,paths    = require('../paths')
+    ,imagemin = require('gulp-imagemin')
+    ,cache    = require('gulp-cache');
 
 // Call Imagemin
 module.exports = gulp.task('imagemin', function () {
   return gulp.src(paths.source.img)
     .pipe(plumber())
-    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe(gulp.dest(paths.build.img));
 });
