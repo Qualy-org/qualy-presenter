5944dbf9901530197f48fa14bd195b62c8c29fea
Matheus Brasil <matheus.brasil10@gmail.com>
2015-10-05 02:56:47 -0300

    Add JS task

---
+++ gulp/tasks/js.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Necessary Plugins
+var gulp          = require('gulp')
+    ,plumber        = require('gulp-plumber')
+    ,paths          = require('../paths')
+    ,uglify         = require('gulp-uglify')
+    ,concat         = require('gulp-concat');
+
+// Call Uglify and Concat JS
+module.exports = gulp.task('js', function(){
+  return gulp.src(paths.source.js)
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.build.js))
+});
