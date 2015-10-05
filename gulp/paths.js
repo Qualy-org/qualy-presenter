617b0e974e31b4c1fbc61994c896236bf5763111
Matheus Brasil <matheus.brasil10@gmail.com>
2015-10-05 02:55:38 -0300

    Modularize paths
    Having paths in a separate modules makes it a lot easier when changing a
    path that is used across many tasks.

---
+++ gulp/paths.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+
+    source: {
+        templates: 'src/templates/**/*.jade',
+        slides: 'src/slides/*.jade',
+        js: 'src/js/**/*.js',
+        styl: 'src/styl/**/*.styl',
+        img: 'src/img/**/*',
+        files: {
+            jade: 'src/templates/index.jade',
+            styl: 'src/styl/main.styl',
+			notes: 'src/js/vendor/notes/notes.html'
+        }
+
+    },
+
+    browserSync: {
+        html: 'build/**/*.html',
+        js: 'build/js/**/*.js',
+        css: 'build/css/**/*.css',
+        img: 'build/img/**/*'
+    },
+
+    build: {
+        html: './build/',
+        js: 'build/js',
+        css: 'build/css',
+        img: 'build/img',
+		notes: 'build/js/vendor/notes/'
+    },
+	
+    deploy: {
+        pages: 'build/**/*',
+        src: './'
+    }
+};
