ecad7902df8140ddd7ebd942413f98f14c3a077d
fernandoGuisso <fguisso@gmail.com>
2016-03-14 17:06:06 -0300

    include slides/*.jade

--- gulp/tasks/jade.js
+++ gulp/tasks/jade.js
@@ -5,12 +5,14 @@ var gulp     = require('gulp')
     ,plumber = require('gulp-plumber')
     ,paths   = require('../paths')
     ,fs      = require('fs')
+    ,glob    = require('gulp-jade-globbing')
     ,jade    = require('gulp-jade');
 
 // Call Jade to compile Templates
 module.exports = gulp.task('jade', function () {
   return gulp.src(paths.source.files.jade)
     .pipe(plumber())
+    .pipe(glob())
     .pipe(jade({
       pretty: true
     }))
