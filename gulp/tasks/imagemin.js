5a6a20decd7f7d845b25aedb1e931bdbfdca73c3
Willian Justen <willianjustenqui@gmail.com>
2016-06-07 19:49:32 -0300

    Edit imagemin task

--- gulp/tasks/imagemin.js
+++ gulp/tasks/imagemin.js
@@ -4,13 +4,12 @@
 var gulp      = require('gulp')
     ,plumber  = require('gulp-plumber')
     ,paths    = require('../paths')
-    ,imagemin = require('gulp-imagemin')
-    ,cache    = require('gulp-cache');
+    ,imagemin = require('gulp-imagemin');
 
 // Call Imagemin
 module.exports = gulp.task('imagemin', function () {
   return gulp.src(paths.source.img)
     .pipe(plumber())
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
     .pipe(gulp.dest(paths.build.img));
 });
